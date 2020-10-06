04f2fb703b3c75c27a1d0e1ad35e4039298d8bfa
seanc <seanc@DESKTOP-Q9PLQT3>
2020-10-06 12:09:01 -0700

    Initial commit

---
+++ Assignment_1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,29 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;	// not needed - included for clarity
+	}
+	
+	//The getTotal method should return the current total
+	int getTotal () {
+		return 0;
+	}
+	
+	//The add method should add the parameter to the total variable
+	public void add (int value) {
+	}
+	
+	//The subtract method should subtract the parameter from the total variable
+	public void subtract (int value) {
+	}
+	
+	public String toString () {
+	}
+	
+	//The clear method should clear your AddingMachine
+	public void clear() {
+	}
+}
